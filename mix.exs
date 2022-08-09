a05b7dabb01fd2c89e499f35eaa5c88d1ac852bd
JuanCaArredono <juan.arredondo@diverza.com>
2022-08-09 19:39:02 +0000

    license currently process

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule LiveViewStudio.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:number, "~> 1.0.1"},
       {:phoenix, "~> 1.5.7"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
