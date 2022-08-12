136ac80ad41ee5ed93e8c18e979912f9cfd57d35
JuanCaArredono <juan.arredondo@diverza.com>
2022-08-12 22:50:12 +0000

    update paginations

--- lib/live_view_studio_web/live/paginate_live.ex
+++ lib/live_view_studio_web/live/paginate_live.ex
@@ -21,8 +21,38 @@ defmodule LiveViewStudioWeb.PaginateLive do
     {:noreply,socket}
   end
 
+  def handle_event("select-per-page", %{"per-page"=>per_page},socket) do
+    per_page=String.to_integer(per_page)
+    socket=
+      push_patch(socket,
+        to: Routes.live_path(
+          socket,
+          __MODULE__,
+          page: socket.assigns.options.page,
+          per_page: per_page
+        )
+      )
+
+    {:noreply,socket}
+
+  end
 
   defp expires_class(donation) do
     if Donations.almost_expired?(donation), do: "eat-now", else: "fresh"
+  end
+
+  defp pagination_link(socket, text, page,per_page,class) do
+
+    live_patch(text,
+      to:
+        Routes.live_path(
+          socket,
+          __MODULE__,
+          page: page,
+          per_page: per_page
+        ),
+        class: class
+    )
+
   end
 end
